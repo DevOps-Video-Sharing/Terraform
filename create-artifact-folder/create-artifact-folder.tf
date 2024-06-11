58e63145b7ed673b6b636f15d8bf73545c456d76
Tạ Đức Bảo <146197243+DucBaoUIT@users.noreply.github.com>
2024-06-11 20:50:17 +0700

    Edit Create Artifact folder

--- create-artifact-folder/create-artifact-folder.tf
+++ create-artifact-folder/create-artifact-folder.tf
@@ -1,6 +1,13 @@
-resource "google_artifact_registry_repository" "my-repo" {
+resource "google_artifact_registry_repository"  "frontend-repo" {
   location      = var.region
-  repository_id = "terraform-test-repository"
-  description   = "test create repository by terraform"
+  repository_id = "frontend"
+  description   = "frontend repository created by terraform"
   format        = "DOCKER"
 }
+resource "google_artifact_registry_repository"  "backend-repo" {
+  location      = var.region
+  repository_id = "backend"
+  description   = "backend repository created by terraform"
+  format        = "DOCKER"
+}
+
